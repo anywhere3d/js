070b39448870543e56d4c3ecf5aa202e8afc9dc8
anywhere3D <anywhere3d@gmail.com>
2018-06-10 14:50:48 +0200

    Update runtime.js

--- runtime.js
+++ runtime.js
@@ -3,17 +3,17 @@
     var debugMode;
     var mouse = new THREE.Vector2();
 
-    var rendererHalfWidth = renderer.domElement.width / 2;
-    var rendererHalfHeight = renderer.domElement.height / 2;
+    var rendererHalfWidth = renderer.domElement.width * 0.5;
+    var rendererHalfHeight = renderer.domElement.height * 0.5;
 
-    groundHelper.visible = debugMode;
-    axisCustomHelper.visible = debugMode;
-
-//  if (debugMode) {       
-    //  controls = new THREE.EditorControls(camera);
-    //  camera.lookAt(controls.center);
-//  }
-
+    groundHelper.visible = true;
+    axisCustomHelper.visible = true;
+/*
+    if (debugMode) {       
+        controls = new THREE.EditorControls(camera);
+        camera.lookAt(controls.center);
+    }
+*/
     animate();
 
     function animate(){
@@ -44,8 +44,8 @@
     $(window).on( "mousemove", onMouseMove );
 
 	function onMouseMove( event ) {
-        //console.log(event);
-		mouse.x = ( event.clientX - rendererHalfWidth );
+
+        mouse.x = ( event.clientX - rendererHalfWidth );
 		mouse.y = ( event.clientY - rendererHalfHeight );
 
 	}
