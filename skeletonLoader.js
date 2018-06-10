4d31efa795efea5bf863540eaa0ba1d0b1f0a238
anywhere3D <anywhere3d@gmail.com>
2018-06-11 00:22:02 +0200

    Update skeletonLoader.js

--- skeletonLoader.js
+++ skeletonLoader.js
@@ -22,32 +22,27 @@
                 throw Error("Error: json not defined.");
 
         //  Local Forage.
-            AW3Dstore.setItem(key, json)
-            .then(function (value) {
-                console.log("value:", value);
+            AW3Dstore.setItem(key, json).then(function (value) {
+                debugMode && console.log("value:", value);
                 return value;
+
+            }).catch( function(err){
+                throw Error(err);
+                
+            }).then(function(json){
+                Avatars[ name ] = initSkinnedAsset( json );
+                return Avatars[ name ];
+
+            }).then(function(asset){
+                loadTextures( asset );
+
+            }).then(function(){
+                sceneAddBody( name );
             });
-
-        }).then(function(json){
-            Avatars[ name ] = initOutfitAsset( json );
-            return Avatars[ name ];
-
-        }).then(function(asset){
-            loadTextures( asset );
-
-        }).then(function(){
-            sceneAddBody( name )
 
         }).fail(function(err){
             console.error(err);
         });
-
-        function addToLocalStorageAvatars(key, data){
-            var object = {};
-            object[key] = data;
-            console.log(object);
-            store.add("Avatars", object);
-        }
 
     }
 
@@ -79,7 +74,6 @@
 
     //  Set imgur url.
         mapOptions.url = imgurQualityUrl( mapOptions );
-    //  debugMode && console.log("skeleton mapOptions.url:", mapOptions.url);
     //  mapOptions.url = "https://i.imgur.com/tjNZqbq.jpg";
 
     //  Load texture.
@@ -93,18 +87,17 @@
             var name  = options.name;
             var index = options.index;
             var asset = options.asset;
-    
-            var img = new Image();
-            img.crossOrigin = "anonymous";
-            $(img).one("load", function (){
-                var texture = new THREE.Texture( img ); // or canvas //
-                texture.name = name;
-                texture.sourceFile = url;
+
+            var texture = new THREE.Texture();
+            texture.name = name;
+            texture.sourceFile = url;
+            var loader = new THREE.ImageLoader();
+            loader.setCrossOrigin( "anonymous" );
+            loader.load( url, function ( image ) {
+                texture.image = image;
+                texture.needsUpdate = true;
                 applyTexture( asset, texture, map, index );
-                $(img).remove();
-            });
-    
-            img.src = url;
+            })
         }
 
     }, function sceneAddBody( name ){
@@ -117,4 +110,27 @@
         scene.add(localPlayer.outfit.direction);
 
     });
+
+    /*
+        function textureMapLoader( options ){
+
+            var url   = options.url;
+            var map   = options.map;
+            var name  = options.name;
+            var index = options.index;
+            var asset = options.asset;
+
+            var img = new Image();
+            img.crossOrigin = "anonymous";
+            $(img).one("load", function (){
+                var texture = new THREE.Texture( img ); // or canvas //
+                texture.name = name;
+                texture.sourceFile = url;
+                applyTexture( asset, texture, map, index );
+                $(img).remove();
+            });
+    
+            img.src = url;
+        }
+    */
 
