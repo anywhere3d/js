4c420f34971caa01523911d172ef7ed0fb69ad6a
anywhere3D <anywhere3d@gmail.com>
2018-06-11 21:35:51 +0200

    Update hmBodyLoader.js

--- hmBodyLoader.js
+++ hmBodyLoader.js
@@ -8,29 +8,7 @@
 //    var assetKey = "aw3d.avatar.male.body";
 //    var assetUrl = assetsFolder + "HM_BodyLayer_ABK04_v02.js";
 
-//  More simple solution. Using service-worker for caching all data.
-
-    function $getMaleBody(options, loadTextures){
-
-        var url  = options.url;
-        var key  = options.key;
-        var name = options.name;
-
-        $.getJSON( url ).then(function(json){
-
-            if (!json) throw Error("json did not defined");
-            Avatars[ name ] = initOutfitAsset( json );
-            return Avatars[ name ];
-
-        }).then(function(asset){
-            loadTextures( asset )
-        }).fail(function(err){
-            console.error(err);
-        });
-
-    }
-
-    $getMaleBody({
+    $getOutfit({
 
         name: "hmBody",
         key : "aw3d.avatar.male.body",
@@ -162,86 +140,7 @@
 
     });
 
-/*
-        function textureMapLoader( options ){
 
-            var url   = options.url;
-            var map   = options.map;
-            var name  = options.name;
-            var index = options.index;
-            var asset = options.asset;
-    
-            var img = new Image();
-            img.crossOrigin = "anonymous";
-            $(img).one("load", function (){
-                var texture = new THREE.Texture( img ); // or canvas //
-                texture.name = name;
-                texture.sourceFile = url;
-                applyTexture( asset, texture, map, index );
-                $(img).remove();
-            });
-    
-            img.src = url;
-        }
-*/
-
-
-
-
-/*!
-* @author anywhere3d
-* http://anywhere3d.org
-* MIT License
-*/
-
-/*!
-*  How to use:
-*  --------------
-*
-*      $getAsset(
-*          options, 
-*          loadTextures( asset ){
-*              ...........
-*          },
-*          optionalfunc1(){
-*              ........... 
-*          },
-*          optionalfunc2(){
-*              ........... 
-*          }
-*      );
-*  
-*  argument "options":
-*    options.key : the key of json data in localStorage,
-*    options.url : the url of asset json file.
-*    options.name: the key name of asset in Avatars object.
-*    You can add additional properties in options if you need.
-*
-*  argument "loadTexture":
-*    a chain callback function of type function( asset ),
-*    used in promise chain to setup the textures of asset maps.
-*    It takes "asset" as argument and you can use it to load 
-*    and apply textures or/and set parameters to asset materials.
-*
-*  You can add additional functions as arguments if you need 
-*  more tasks in the promise chain.
-*
-*  The function looks if window support localeStorage and if so,
-*  try to upload the json data from localStorage using the 
-*  "options.key" argument. If key does not exist in localStorage 
-*  then fetch the data from the json file using the $.getJSON(url) 
-*  method with "options.url" as "url" argument, and store the 
-*  json data to the localStorage under the key "options.key".
-*  
-*  Use the "loadTextures()" chain callback to load textures and 
-*  set asset material properties. You can use an additional callback 
-*  like "sceneAddAsset()" if you want to add the asset in the scene.
-*  You also can use as manny additional chain callbacks as you need.
-*  (by modifing the promise chain of the $getAsset function).
-*
-*  Useful to know: "$getAsset()" use the "store2.js" library
-*  ["https://github.com/nbubna/store"] to manage localStorage.
-*/
 
 /*
     function $getMaleBody(options, loadTextures){
@@ -250,56 +149,17 @@
         var key  = options.key;
         var name = options.name;
 
-        if ( !!window.localStorage ){
-    
-            if ( store.has(key) ){
+        $.getJSON( url ).then(function(json){
 
-                console.log("loading from store key:", key);
+            if (!json) throw Error("json did not defined");
+            Avatars[ name ] = initOutfitAsset( json );
+            return Avatars[ name ];
 
-                $.Deferred().resolve( store(key) ).then(function(json){
-
-                    if (!json) throw Error("json did not defined");
-                    Avatars[ name ] = initOutfitAsset( json );
-                    return Avatars[ name ];
-
-                }).then(function(asset){
-                    loadTextures( asset )
-                }).fail(function(err){
-                    console.error(err);
-                });
-
-            } else {
-
-                $.getJSON( url ).then(function(json){
-
-                    if (!json) throw Error("json did not defined");
-                    Avatars[ name ] = initOutfitAsset( json );
-                    store(key, json);
-                    return Avatars[ name ];
-                
-                }).then(function(asset){
-                    loadTextures( asset )
-                }).fail(function(err){
-                    console.error(err);
-                });
-
-            }
-
-        } else {
-
-            $.getJSON( url ).then(function(json){
-
-                if (!json) throw Error("json did not defined");
-                Avatars[ name ] = initOutfitAsset( json );
-                return Avatars[ name ];
-
-            }).then(function(asset){
-                loadTextures( asset )
-            }).fail(function(err){
-                console.error(err);
-            });
-
-        }
+        }).then(function(asset){
+            loadTextures( asset )
+        }).fail(function(err){
+            console.error(err);
+        });
 
     }
 */
