fe6a9321d6f8486de856da3ec464bd1730c62195
anywhere3D <anywhere3d@gmail.com>
2018-06-11 23:59:32 +0200

    Update fmTshirtLoader.js

--- fmTshirtLoader.js
+++ fmTshirtLoader.js
@@ -24,6 +24,7 @@
                 debugMode && console.log("Outfit:", "Getting from AW3D Store");
 
                 Avatars[ name ] = initSkinnedAsset( result );
+                Avatars[ name ].geometry.sourceFile = url;
                 loadTextures( Avatars[ name ] );
                 loadFrontTexture( Avatars[ name ] );
 
@@ -56,6 +57,7 @@
                     } else {
                         console.log("success:", result);
                         Avatars[ name ] = initSkinnedAsset( result );
+                        Avatars[ name ].geometry.sourceFile = url;
                         loadTextures( Avatars[ name ] );
                         loadFrontTexture( Avatars[ name ] );
                     }
