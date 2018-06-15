d3b116fc0d57eaf73cd1f188fa9b1250f21dd010
anywhere3d <anywhere3d@gmail.com>
2018-06-15 14:49:15 +0200

    Add AnimationHandler

--- AW3D.js
+++ AW3D.js
@@ -738,3 +738,251 @@
     };
 
 
+
+
+//  AW3D AnimationHandler.js
+
+    /*!
+    * @author anywhere3d
+    * http://anywhere3d.org
+    * MIT License
+    */
+
+
+//  Reset THREE.AnimationHandler.animations array.
+    THREE.AnimationHandler.animations.length = 0;
+    AnimationManager = THREE.AnimationHandler;
+
+    AW3D.AnimationHandler = function ( mesh, gender ) {
+
+        this.mesh = mesh;
+        this.gender = gender; // IMPORTANT //
+        this.actions = {};
+
+    //  This create the animations of skinned mesh. 
+        this.reloadActions(); // IMPORTANT //
+
+    };
+    
+    AW3D.AnimationHandler.prototype = {
+
+        constructor: AW3D.AnimationHandler,
+
+        findAction: function(action){
+            return THREE.AnimationHandler.animations.filter( function(animation){
+                return (animation == action); // boolean.
+            }); // BE CAREFULL: returns new array with resutls.
+        },
+
+        findByUuid: function( name ){
+            return THREE.AnimationHandler.animations.filter( function(animation){
+                return (animation.uuid == this.actions[ name ].uuid); // boolean.
+            }); // BE CAREFULL: returns new array with resutls.
+        },
+
+        findByName: function( name ){
+            return THREE.AnimationHandler.animations.filter( function(animation){
+                return (animation.data.name == name); // boolean.
+            }); // BE CAREFULL: returns new array with resutls.
+        },
+
+    //  To stop an animation, find the animation in
+    //  THREE.AnimationHandler.animations and stop it from there.
+
+        stop: function stop(){
+            var self = this;
+            Object.keys( self.actions ).forEach(function(name, i){
+                var action = self.actions[name];
+                self.findAction(action).forEach(function(animation){
+                    animation.stop();
+                });
+            });
+        },
+
+    //  To delete an action, stop the animation in 
+    //  THREE.AnimationHandler.animations and then delete it from this.actions.
+
+        delete: function( name ){
+            var action = this.actions[ name ];
+            this.findAction( action ).forEach(function(animation){
+                animation.stop();
+            });
+            delete this.actions[ name ];
+        },
+
+        reset: function reset(){
+            for (var i in arguments){
+                var name = arguments[i];
+                this.actions[ name ].weight = 1;
+                this.actions[ name ].currentTime = 0;
+                this.actions[ name ].timeScale = this.actions[name].data.length;
+            }
+        },
+
+        resetAll: function(){
+            var self = this;
+            Object.keys( self.actions ).forEach(function(name, i){
+                self.reset( name );
+            });
+        },
+
+        deleteAll: function(){
+            var self = this;
+            Object.keys( self.actions ).forEach(function(name, i){
+                self.delete[ name ]
+            });
+        },
+
+        play: function play(){
+            for (var i in arguments){
+                var name = arguments[i];
+                if ( !this.actions[ name ] ) return;
+                this.actions[ name ].play(0);
+            }
+        },
+
+    //  To pause an animation, find the animation 
+    //  in THREE.AnimationHandler.animations and set timeScale to 0.
+    
+        pause: function pause(){
+            for (var i in arguments){
+                var name = arguments[i];
+                var action = this.actions[ name ];
+                this.findAction( action ).forEach(function(animation){
+                    animation.timeScale = 0;
+                });
+            }
+        },
+
+    //  To unpause an animation, find the animation 
+    //  in THREE.AnimationHandler.animations and set timeScale to animation.data.length.
+
+        continue: function(){
+            for (var i in arguments){
+                var name = arguments[i];
+                var action = this.actions[ name ];
+                this.findAction( action ).forEach(function(animation){
+                    animation.timeScale = animation.data.length;
+                });
+            }
+        },
+
+        weightOff: function(){
+            for (var i in arguments){
+                var name = arguments[i];
+                var action = this.actions[ name ];
+                this.findAction( action ).forEach(function(animation){
+                    animation.weight = 0;
+                });
+            }
+        },
+
+        weightOn: function(){
+            for (var i in arguments){
+                var name = arguments[i];
+                var action = this.actions[ name ];
+                this.findAction( action ).forEach(function(animation){
+                    animation.weight = 1;
+                });
+            }
+        },
+
+        idle: function idle(){
+            this.actions.idle.play(0);
+        },
+
+        jump: function jump(){
+            this.actions.jump.play(0);
+        },
+
+        run: function run(){
+            this.actions.run.play(0);
+        },
+
+        walk: function walk(){
+            this.actions.walk.play(0);
+        },
+    
+    //  --------------------------------------------------------  //
+
+    //  IMPORTANT: This create the animations of skinned mesh.
+
+        loadAction: function(){
+
+            for (var i in arguments){
+                var name = arguments[i];
+            //  var data = Animations[ name ];
+                var data;
+                switch(this.gender){
+                    case "male":
+                        data = MaleAnimations[ name ];
+                        break;
+                    case "female":
+                        data = FemaleAnimations[ name ];
+                        break;
+                    case false:
+                        data = Animations[ name ];
+                        break;
+                    default:
+                        data = Animations[ name ];
+                }
+            
+                var action = new THREE.Animation( this.mesh, data );
+                action.uuid = THREE.Math.generateUUID();
+                action.weight = 1;
+                action.currentTime = 0;
+                action.timeScale = 1;   // action.data.length;
+                this.actions[ name ] = action;
+            }
+        },
+
+    //  --------------------------------------------------------  //
+
+        reloadActions: function(){
+            var self = this;
+
+            this.stop();
+            this.deleteAll();
+            this.actions = {};
+
+            if (!!this.gender && this.gender == "male") {
+                Object.keys( MaleAnimations ).forEach(function(name, i){
+                    self.loadAction( name );
+                });
+            }
+            
+            if (!!this.gender && this.gender == "female") {
+                Object.keys( FemaleAnimations ).forEach(function(name, i){
+                    self.loadAction( name );
+                });
+            }
+
+            if ( !this.gender ) {
+                Object.keys( Animations ).forEach(function(name, i){
+                    self.loadAction( name );
+                });
+            }
+
+            if ( !!this.gender && this.gender != "male" && this.gender != "female" ){
+                console.warn("AW3D.AnimationHandler:",
+                    "reloadActions(" + this.gender + ")", 
+                    "Gender exists but is not male or female."
+                );
+            }
+
+            if (!!this.actions.jump) this.actions.jump.loop = false;
+        },
+
+    //  --------------------------------------------------------  //
+
+        promise: function( fn ){
+            return new Promise(function(resolve, reject){
+                if ( !fn ) resolve();
+                if ( fn instanceof Function ) resolve( fn() );
+                else resolve();
+            });
+        },
+
+    };
+
+
