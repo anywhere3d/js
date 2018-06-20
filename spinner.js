21dfe5a984ae62094cd91dd2a7e18d5274500e2d
anywhere3D <anywhere3d@gmail.com>
2018-06-20 11:58:24 +0200

    Update spinner.js

--- spinner.js
+++ spinner.js
@@ -2,8 +2,8 @@
 
 var spinnerSelector = "#spinner";
 var spinnerLoadingSelector = ".spinner-loading";
-var spinner_html = '<div id="spinner" class="spinner-loading"><img src="/img/load.gif"></div>';
-var spinner      = '<div id="spinner" class="spinner-loading"><img src="/img/load.gif"></div>';
+var spinner_html = '<div id="spinner" class="spinner-loading"><img src="https://i.imgur.com/7qG1W04.gif"></div>';
+var spinner      = '<div id="spinner" class="spinner-loading"><img src="https://i.imgur.com/7qG1W04.gif"></div>';
 //  var spinner = $(spinner_html)[0];
 
         
