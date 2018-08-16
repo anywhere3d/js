888fd40accf16f40356ab6f5fd8c0dad0aa2cd5b
anywhere3D <anywhere3d@gmail.com>
2018-08-16 18:22:36 +0200

    Delete spinner.js

--- spinner.js
+++
@@ -1,9 +0,0 @@
-//  spinner.js
-
-var spinnerSelector = "#spinner";
-var spinnerLoadingSelector = ".spinner-loading";
-var spinner_html = '<div id="spinner" class="spinner-loading"><img src="https://i.imgur.com/7qG1W04.gif"></div>';
-var spinner      = '<div id="spinner" class="spinner-loading"><img src="https://i.imgur.com/7qG1W04.gif"></div>';
-//  var spinner = $(spinner_html)[0];
-
-        
